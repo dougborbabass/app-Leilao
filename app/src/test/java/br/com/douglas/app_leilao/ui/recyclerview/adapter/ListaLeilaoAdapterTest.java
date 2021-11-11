8f41a2c775844a7ea41f3efd57ecd6ac420c09c7
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-11-11 14:36:04 -0300

    adicionado mockit para mockar context android

--- app/src/test/java/br/com/douglas/app_leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
+++ app/src/test/java/br/com/douglas/app_leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -1,6 +1,9 @@
 package br.com.douglas.app_leilao.ui.recyclerview.adapter;
 
+import android.content.Context;
+
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -15,7 +18,10 @@ public class ListaLeilaoAdapterTest {
 
     @Test
     public void deve_AtualizarAListaDeLeiloes_QuandoReceberListaDeLeiloes() {
-        ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(null);
+
+        Context context = Mockito.mock(Context.class);
+        ListaLeilaoAdapter adapter = Mockito.spy(new ListaLeilaoAdapter(context));
+        Mockito.doNothing().when(adapter).atualizaLista();
 
         adapter.atualiza(new ArrayList<>(Arrays.asList(
                 new Leilao("Console"),
