9e339ab8d094130b2c3dace176b152b59fb364a0
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-11-11 15:17:46 -0300

    refactor test

--- app/src/test/java/br/com/douglas/app_leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
+++ app/src/test/java/br/com/douglas/app_leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -3,7 +3,12 @@ package br.com.douglas.app_leilao.ui.recyclerview.adapter;
 import android.content.Context;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -13,16 +18,20 @@ import br.com.douglas.app_leilao.model.Leilao;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ListaLeilaoAdapterTest {
+
+    @Mock
+    private Context context;
+    @Spy
+    private final ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
 
     @Test
     public void deve_AtualizarAListaDeLeiloes_QuandoReceberListaDeLeiloes() {
-
-        Context context = Mockito.mock(Context.class);
-        ListaLeilaoAdapter adapter = Mockito.spy(new ListaLeilaoAdapter(context));
-        Mockito.doNothing().when(adapter).atualizaLista();
-
+        doNothing().when(adapter).atualizaLista();
         adapter.atualiza(new ArrayList<>(Arrays.asList(
                 new Leilao("Console"),
                 new Leilao("Computdaor")
@@ -30,6 +39,7 @@ public class ListaLeilaoAdapterTest {
 
         int quantidadeLeiloesDevolvida = adapter.getItemCount();
 
+        verify(adapter).atualizaLista();
         assertThat(quantidadeLeiloesDevolvida, is(2));
     }
 }
