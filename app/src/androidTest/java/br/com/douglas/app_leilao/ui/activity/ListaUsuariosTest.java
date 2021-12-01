cbfb095def8c43edb15726540a222976ec042514
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-12-01 10:34:12 -0300

    refactor e clean code no teste

--- app/src/androidTest/java/br/com/douglas/app_leilao/ui/activity/ListaUsuariosTest.java
+++ app/src/androidTest/java/br/com/douglas/app_leilao/ui/activity/ListaUsuariosTest.java
@@ -6,11 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -19,7 +17,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import br.com.douglas.app_leilao.BuildConfig;
 import br.com.douglas.app_leilao.R;
@@ -30,17 +27,13 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
-@RunWith(AndroidJUnit4.class)
 public class ListaUsuariosTest {
 
     @Rule
@@ -53,74 +46,35 @@ public class ListaUsuariosTest {
     }
 
     @Test
-    public void listaUsuariosTest() {
-        ViewInteraction actionMenuItemView = onView(
-                allOf(withId(R.id.lista_leilao_menu_usuarios), withContentDescription("Usuários"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.action_bar),
-                                        1),
-                                0),
-                        isDisplayed()));
-        actionMenuItemView.perform(click());
+    public void deve_AparecerUsuarioNaListaDeUsuarios_QuandoCadastrarUmUsuario() {
+        onView(allOf(withId(R.id.lista_leilao_menu_usuarios),
+                isDescendantOfA(withId(R.id.action_bar)),
+                isDisplayed()))
+                .perform(click());
 
-        ViewInteraction floatingActionButton = onView(
-                allOf(withId(R.id.lista_usuario_fab_adiciona),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        floatingActionButton.perform(click());
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDescendantOfA(withId(android.R.id.content)),
+                isDisplayed()))
+                .perform(click());
 
-        ViewInteraction textInputEditText = onView(
-                allOf(childAtPosition(
-                        childAtPosition(
-                                withId(R.id.form_usuario_nome),
-                                0),
-                        1),
-                        isDisplayed()));
-        textInputEditText.perform(replaceText("Douglas"), closeSoftKeyboard());
+        onView(allOf(withId(R.id.form_usuario_nome_edit_text),
+                isDescendantOfA(withId(R.id.form_usuario_nome)),
+                isDisplayed()))
+                .perform(replaceText("Douglas"), closeSoftKeyboard());
 
-        ViewInteraction materialButton = onView(
-                allOf(withId(android.R.id.button1), withText("Adicionar"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-        materialButton.perform(scrollTo(), click());
+        onView(allOf(withId(android.R.id.button1),
+                withText("Adicionar"),
+                isDisplayed()))
+                .perform(scrollTo(), click());
 
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.item_usuario_id_com_nome), withText("(1) Douglas"),
-                        withParent(withParent(withId(R.id.lista_usuario_recyclerview))),
-                        isDisplayed()));
-        textView.check(matches(withText("(1) Douglas")));
+        onView(allOf(withId(R.id.item_usuario_id_com_nome),
+                isDisplayed()))
+                .check(matches(withText("(1) Douglas")));
     }
 
     @After
     public void tearDown() {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         appContext.deleteDatabase(BuildConfig.BANCO_DE_DADOS);
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
